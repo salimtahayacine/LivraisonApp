c7a11f338734b0b51a5d8619bbb0885539ccb595
salimtahayacine <salimtahayacine@gmail.com>
2022-06-26 14:34:13 -0700

    permier push

---
+++ sass.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    includePaths : [
+        "node_modules/ionic-angular/themes",
+        "node_modules/ioniccons/scss",
+        "node_modules/ionic-angular/fonts",
+        "node_modules/bootstrap/scss"
+    ]
+};
